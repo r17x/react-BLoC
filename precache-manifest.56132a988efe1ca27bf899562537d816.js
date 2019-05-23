18e0e4c0fc8347b8afbf39b65c97a25b3fe95d83
ri7nz <ri7nz.labs@gmail.com>
2019-05-23 23:58:36 +0700

    Updates

---
+++ precache-manifest.56132a988efe1ca27bf899562537d816.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed40359a6afc4f665ed1b3ac5ff5645b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "35d828838044518940ac",
+    "url": "/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "a656c59fa2f46733cb7e",
+    "url": "/static/js/2.7328e930.chunk.js"
+  },
+  {
+    "revision": "35d828838044518940ac",
+    "url": "/static/js/main.458e401e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
